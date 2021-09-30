bb90d4a49000681a5aaea83319b81a380a947231
Gaston Jolinont <falsee63@gmail.com>
2021-09-30 10:09:03 -0300

    Aprendiendo Tipos de datos NO primitivos

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+      // Tipo de dato no primitivo Integer
+
+        Integer numero = null; // podemos dar valor null a variables no primitivas
+
+        //al escribir numero. se extenderan varios metodos
+
+        System.out.println("El numero es:"+numero);
+
+
+        //CADENAS
+
+        String palabra = "Hola que tal"; // se usan comillas dobles las simples son para los caracteres.
+
+        //al escribir palabra. se extenderan varios metodos
+
+        System.out.println(palabra);
+
+
+    }
+}
